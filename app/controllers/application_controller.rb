1148517012031952bbd0b768ed95b723444967f3
Olivia Kesler <oakesler@protonmail.ch>
2020-01-10 15:48:05 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,9 @@ class ApplicationController < Sinatra::Base
     erb :"application/index"
   end
   
-  get '/figures' do 
-    erb :"figures/index"
-  end
+  #get '/figures' do 
+    #erb :"figures/index"
+  #end
   
   get '/landmarks' do
     erb :"landmarks/index"
@@ -18,16 +18,17 @@ class ApplicationController < Sinatra::Base
     erb :"titles/index"
   end
   
-  get '/figures/new' do 
-    erb :"figures/new"
-  end
+  #get '/figures/new' do 
+    #erb :"figures/new"
+  #end
   
-  post '/figures' do
-    @figure = Figure.new[name: params[name]]
-    redirect to "/figures/#{params[:id]}"
-  end
+  #post '/figures' do
+    #@figure = Figure.new[name: params[name]]
+    #redirect to "/figures/#{params[:id]}"
+  #end
   
-  get '/figures/:id' do 
-    @figure = Figure.all.find(params[:id])
-    erb :"figures/show"
+  #get '/figures/:id' do 
+    #@figure = Figure.all.find(params[:id])
+    #erb :"figures/show"
+  #end
 end
--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,19 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  get '/figures' do 
+    erb :"figures/index"
+  end
+  
+  get '/figures/new' do 
+    erb :"figures/new"
+  end
+  
+  post '/figures' do
+    @figure = Figure.new[name: params[name]]
+    redirect to "/figures/#{params[:id]}"
+  end
+  
+  get '/figures/:id' do 
+    @figure = Figure.all.find(params[:id])
+    erb :"figures/show"
+  end
 end
