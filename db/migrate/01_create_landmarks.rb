370807986ee0e297b519b989af5cf874e7805e38
Olivia Kesler <oakesler@protonmail.ch>
2020-01-10 14:41:42 +0000

    Done.

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,3 +1,14 @@
 class CreateLandmarks < ActiveRecord::Migration
-  raise 'Write CreateLandmarks migration here'
+  #raise 'Write CreateLandmarks migration here'
+  def up
+    create_table :landmarks do |t|
+      t.string :name
+      t.string :figure_id
+      t.string :year_completed
+    end
+  end
+  
+  def down
+    drop_table :landmarks
+  end
 end
